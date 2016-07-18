1de082451d9e2588c0c795330dcb63da540277dd
yoshi <marcin.niewinski@hotmail.com>
2016-07-18 23:07:15 +0200

    uzupełniona klasa DownvoterStrategy

--- Test/src/Strategies/DownvoterStrategy.java
+++ Test/src/Strategies/DownvoterStrategy.java
@@ -1,24 +1,77 @@
 package Strategies;
 
 public class DownvoterStrategy {
-	private int minimumStaminaToAnswer=100;
-	private int maxReputationDifferenceToDownVote =1;
-	
-	public DownvoterStrategy(int i, int j, int k, int l, int m) {
-		// TODO Auto-generated constructor stub
+
+	private int rankObjective;
+	private int minTotalAnswerCount; 
+	private int lastPositionForNewAnswer;
+	private int maxDifferenceForNewAnswer;
+	private int maxDifferenceToTakeAction;
+
+	public DownvoterStrategy(int rankObjective, int minTotalAnswerCount, int lastPositionForNewAnswer, int maxDifferenceForNewAnswer, int maxDifferenceToTakeAction) {
+		setRankObjective(rankObjective);
+		setMinTotalAnswerCount(minTotalAnswerCount);
+		setLastPositionForNewAnswer(lastPositionForNewAnswer);
+		setMaxDifferenceForNewAnswer(maxDifferenceForNewAnswer);
+		setMaxDifferenceToTakeAction(maxDifferenceToTakeAction);
 	}
-	public int getMinimumStaminaToAnswer() {
-		return minimumStaminaToAnswer;
+
+	private void setRankObjective(int rankObjective) {
+		this.rankObjective = rankObjective;
 	}
-	public void setMinimumStaminaToAnswer(int minimumStaminaToAnswer) {
-		this.minimumStaminaToAnswer = minimumStaminaToAnswer;
+
+	private void setMinTotalAnswerCount(int minTotalAnswerCount) {
+		this.minTotalAnswerCount = minTotalAnswerCount;
 	}
-	public int getMaxReputationDifferenceToDownVote() {
-		return maxReputationDifferenceToDownVote;
+
+	private void setLastPositionForNewAnswer(int lastPositionForNewAnswer) {
+		this.lastPositionForNewAnswer = lastPositionForNewAnswer;
 	}
-	public void setMaxReputationDifferenceToDownVote(
-			int maxReputationDifferenceToDownVote) {
-		this.maxReputationDifferenceToDownVote = maxReputationDifferenceToDownVote;
+
+	private void setMaxDifferenceForNewAnswer(int maxDifferenceForNewAnswer) {
+		this.maxDifferenceForNewAnswer = maxDifferenceForNewAnswer;
 	}
-	
+
+	private void setMaxDifferenceToTakeAction(int maxDifferenceToTakeAction) {
+		this.maxDifferenceToTakeAction = maxDifferenceToTakeAction;
+	}
+
+	/**
+	 * @return the rankObjective (docelowa pozycja odpowiedzi o któr¹ walczy downvoter)
+	 */
+	public int getRankObjective() {
+		return rankObjective;
+	}
+
+	/**
+	 * @return the minTotalAnswerCount (minimalna liczba odpowiedzi w w¹tku do podjêcia akcji przez downvotera)
+	 */
+	public int getMinTotalAnswerCount() {
+		return minTotalAnswerCount;
+	}
+
+	/**
+	 * @return the lastPositionForNewAnswer (ostatnie miejsce na jakie mo¿e
+	 *         zostaæ wstawiona odpowiedŸ downvotera).
+	 */
+	public int getLastPositionForNewAnswer() {
+		return lastPositionForNewAnswer;
+	}
+
+	/**
+	 * @return the maxDifferenceForNewAnswer (maksymalna strata punktowa do
+	 *         najbli¿szej odpowiedzi do której downvoter wstawia w³asny post).
+	 */
+	public int getMaxDifferenceForNewAnswer() {
+		return maxDifferenceForNewAnswer;
+	}
+
+	/**
+	 * @return the maxDifferenceToTakeAction (strata punktowa do najbli¿szej
+	 *         odpowiedzi od której downvoter nie podejmuje ¿adnej akcji)
+	 */
+	public int getmaxDifferenceToTakeAction() {
+		return maxDifferenceToTakeAction;
+	}
+
 }
--- Test/src/Strategies/SocietyStrategies.java
+++ Test/src/Strategies/SocietyStrategies.java
@@ -7,11 +7,12 @@ public class SocietyStrategies extends DefaultContext<Object>{
 	public SocietyStrategies(){
 		super("SocietyStrategies");
 		
-		System.out.println("Society strategies context loaded");	
-		Long[] a1 =  {3L,6L,10L,71L};
-		String[] v1 =  {"3:1,2","7:3","9:1","11:4","64:3"};
+		System.out.println("Society strategies context loaded");
 		
-		SocietyStrategy s1 = new SocietyStrategy(a1,v1,100L);			
+		Long[] a1 =  {3L,6L,10L,71L};
+		String[] v1 =  {"3:1,2","7:3","9:1","11:4","64:3"};		
+		SocietyStrategy s1 = new SocietyStrategy(a1,v1,100L);	
+		
 		this.add(s1);
 	}
 }
